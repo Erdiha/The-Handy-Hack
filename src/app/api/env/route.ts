365fdf67f414fc8d6f97fd1812ad5c96e5477886
erdiha <erdiha@gmail.com>
2025-09-01 12:23:41 -0700

    debug

---
+++ src/app/api/env/route.ts
@@ -0,0 +1,15 @@
+// app/api/env/route.ts
+import { NextResponse } from "next/server";
+
+export function GET() {
+  return NextResponse.json({
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ? "OK" : "MISSING",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL ? "OK" : "MISSING",
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL ? "OK" : "MISSING",
+    DATABASE_URL: process.env.DATABASE_URL ? "OK" : "MISSING",
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME ? "OK" : "MISSING",
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY ? "OK" : "MISSING",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET ? "OK" : "MISSING",
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL ? "OK" : "MISSING",
+  });
+}
